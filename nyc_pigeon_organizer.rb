de975896e7df57e06c796179e7dd3c777cbe7399
npatel111 <nitimaheshpatel@gmail.com>
2016-08-18 13:30:37 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,13 +4,13 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   data.each do |attribute, info|
-    info. each do |attribute2, names|
+    info.each do |trait, names|
       names.each do |name|
         pigeon_list[name] ||= {}
-          pigeon_list[name][attribute.to_sym] ||= []
-            pigeon_list[name][attribute.to_sym] << attribute2.to_s
+        pigeon_list[name][attribute] ||= []
+        pigeon_list[name][attribute] << trait.to_s
       end
     end
   end
-  pigeon_list    
+  pigeon_list
 end
